993517a87fb3cafda32b0c2c770dbd38eb392b87
ncabelin@gmail.com <ncabelin@gmail.com>
2015-12-31 10:51:31 -0800

    JSON Resume
    Udacity Front-end nanodegree Project #2

---
+++ JSONResume/js/resumeBuilder.js
@@ -0,0 +1,83 @@
+var bio = {
+	"name": "Neptune Michael Cabelin",
+	"role": "Software Engineer / Full Stack Web Developer",
+	"contact-info": {
+		"phone": "661-513-7773",
+		"location": "Palmdale, CA"
+	},
+	"picture-URL": "https://cloud.githubusercontent.com/assets/15892944/12047610/1f908b38-ae83-11e5-9d21-ef53cd376aed.jpg",
+	"welcome-message": "Hi, welcome to my profile page, feel free to contact me for more info.",
+	"skills": ["HTML", "CSS", "Javascript", "JQuery", "Bootstrap", "ASP.NET", "Ruby on Rails", "PHP"]
+};
+
+var work = {
+	"jobs": [{
+		"employer": "Annex Healthcare Providers",
+		"title": "Licensed Vocational Nurse",
+		"location": "Encino, CA",
+		"dates": "01/2014-present",
+		"description": "Provided subacute level care for quadruplegic, epileptic, ALS & muscular dystrophy ventilator dependent patients."
+	}, {
+		"employer": "Heavenly Home Health",
+		"title": "Licensed Vocational Nurse",
+		"location": "Glendale, CA",
+		"dates": "08/2012 to 01/2014",
+		"description": "Provided subacute level care for quadruplegic, epileptic, ALS & muscular dystrophy ventilator dependent patients."
+	}, {
+		"employer": "Memorial University Medical Center",
+		"title": "Licensed Practical Nurse",
+		"location": "Rincon, GA",
+		"dates": "03/2011 to 04/2012",
+		"description": "Provided SC & IM injection medications, immunizations, phleobotomy services, glucose checks, PT/INR checks in a medical office setting."
+	}, {
+		"employer": "Caremeridian",
+		"title": "Licensed Vocational Nurse",
+		"location": "Northridge, CA",
+		"dates": "08/2010 to 02/2011",
+		"description": "Provided subacute level care for quadruplegic, epileptic, ALS & muscular dystrophy ventilator dependent patients."
+	}, {
+		"employer": "Unicom Supply",
+		"title": "Logistics Manager / Sales Representative",
+		"location": "Los Angeles, CA",
+		"dates": "03/2007 to 11/2008",
+		"description": "Managed fire alarm & security supplies logistics and handled customer sales accounts"
+	}, {
+		"employer": "ACA Video Philippines",
+		"title": "Operations Manager",
+		"location": "Ormoc City, Philippines",
+		"dates": "03/2001 to 10/2006",
+		"description": "Managed video rental store, launched marketing campaigns & performed sales analyses"
+	}]
+}
+
+var projects = {
+	"project": [{
+		"title": "My Portfolio Website",
+		"dates": "12/2015",
+		"description": "Portfolio to showcase ability to implement HTML5,CSS3,JQuery & Bootstrap technologies",
+		"imageS": ["https://cloud.githubusercontent.com/assets/15892944/12048463/4578b5b6-ae90-11e5-94d8-a4282c274241.jpg", "http://placehold.it/150x150"]
+	}, {
+		"title": "Responsive Website",
+		"dates": "12/2015",
+		"description": "Portfolio to showcase ability to implement responsive website design",
+		"imageS": ["http://placehold.it/150x150", "http://placehold.it/150x150"]
+	}]
+}
+
+var education = {
+	"schools": [{
+		"name": "Casa Loma College",
+		"location": "Van Nuys, CA",
+		"degree": "Certificate",
+		"majors": "Licensed Vocational Nurse",
+		"dates": "01/2009 to 01/2010",
+		"url": "http://casalomacollege.edu"
+	}, {
+		"name": "De La Salle University",
+		"location": "Manila, Philippines",
+		"degree": "Bachelor of Arts",
+		"majors": "Economics",
+		"dates": "06/1997 to 03/2001",
+		"url": "http://dlsu.edu.ph"
+	}]
+}
