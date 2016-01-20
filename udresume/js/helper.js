b9738783d5ea541a062f42543f024e7445607288
ncabelin@gmail.com <ncabelin@gmail.com>
2016-01-20 05:59:14 -0800

    x

--- udresume/js/helper.js
+++ udresume/js/helper.js
@@ -1,5 +1,5 @@
-var HTMLheaderName = '<h1 id="header" class="name head centerSmall">%data%</h1>';
-var HTMLheaderRole = '<p class="white-text spacing centerSmall">%data%</p><hr>';
+var HTMLheaderName = '<h1 class="spacing head flex-item">%data%</h1>';
+var HTMLheaderRole = '<p class="white-text">%data%</p><hr>';
 
 var HTMLcontactGeneric = '<li class="flex-item"><span class="orange-text">%contact%</span><span class="white-text">%data%</span></li>';
 var HTMLmobile = '<li class="flex-item"><span class="orange-text">mobile : </span><span class="white-text">%data%</span></li>';
