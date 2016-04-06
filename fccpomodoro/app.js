a46bfca380efa0ddd95357bd5950ec761f264383
ncabelin@gmail.com <ncabelin@gmail.com>
2016-04-05 22:08:45 -0700

    UI improvement

--- fccpomodoro/app.js
+++ fccpomodoro/app.js
@@ -1,112 +1,155 @@
+$(function() {
 
+  var sessionLength = $("#sessionLength");
+  var sessionBreak = $('#sessionBreak');
+  var sliderSession = $("#sliderSession");
+  var sliderBreak = $("#sliderBreak");
+  var sliders = $(".sliders");
+  var amount = $("#amount"); // 
+  var amount2 = $("#amount2"); // 
+  var amount3 = $("#amount3"); // break seconds
+  var amount4 = $("#amount4"); // session seconds 
+  var start = false;
+  var bell = document.getElementById("bell");
+  var civil = document.getElementById("civil");
+  var click = document.getElementById("click");
+  var status = $("#status");
+  var startTime = $("#startTime");
+  var pauseTime = $("#pauseTime");
+  var clearTime = $("#clearTime");
+  var myVar, timeMinutes, timeSeconds, initialTime, sliderSessionSet, sliderBreakSet;
+  var session = true;
+  var pause = false;
+  var sessT = $(".sess");
+  var breakT = $(".break");
+  var once = false;
 
-
-
-var myVar, timeMinutes, timeSeconds, breakTime, sessionBreak, sessionTime, pushedOnce;
-  var audio2 = document.getElementById("civil");
-  var initialTime = 1500;
-  pushedOnce = false;
-  breakTime = 300;
-  sessionBreak = false;
-  breakMinutes = Math.floor(breakTime/60);
-  timeMinutes = Math.floor(initialTime/60);
-  function displaySession() {
-    sessionTime = timeMinutes;
-    if (sessionTime == 1) {
-    $("#sessionLength").html(" "+sessionTime + " minute");
-      } else {
-    $("#sessionLength").html(" "+sessionTime + " minutes");
-      }
-  }
-  displaySession();
-  function displayBreak () {
-  breakMinutes = Math.floor(breakTime/60);
-  if (breakMinutes == 1) {
-  $("#breakLength").html(" "+breakMinutes + " minute");
-    } else {
-  $("#breakLength").html(" "+breakMinutes + " minutes");
+  sliderSession.slider({
+    range: "max",
+    min: 1,
+    max: 60,
+    value: 25,
+    slide: function(event, ui) {
+      amount.val(ui.value)
     }
+  });
+
+  sliderBreak.slider({
+    range: "max",
+    min: 1,
+    max: 60,
+    value: 5,
+    slide: function(event, ui) {
+      amount2.val(ui.value)
+    }
+  });
+
+  function reset() {
+    sliderSessionSet = sliderSession.slider("value");
+    sliderBreakSet = sliderBreak.slider("value");
+    amount.val( sliderSessionSet )
+    amount2.val( sliderBreakSet )
+    amount3.val(0)
+    amount4.val(0)
   }
-  function printTime() {
+
+  reset()
+
+  function displayTime() {
     timeMinutes = Math.floor(initialTime/60);
     timeSeconds = initialTime - timeMinutes * 60;
-  }
-  function displayTime() {
-    if (timeMinutes == 1) {
-    $("#timeNow").html(timeMinutes + " minute and " + timeSeconds + " seconds left");
-  } else {
-    $("#timeNow").html(timeMinutes + " minutes and " + timeSeconds + " seconds left");
-  }
-  }
-  function startTimeButton() {
-    if (pushedOnce == false) {
-    var audio = document.getElementById("bell");
-    audio.play();
-    myVar = setInterval(beginTime, 1000);
-    pushedOnce = true;
+    if (session) {
+      amount.val(timeMinutes)
+      amount4.val(timeSeconds)
+    } else {
+      amount2.val(timeMinutes)
+      amount3.val(timeSeconds)
     }
   }
+
+  pauseTime.css("display","none")
+
+  startTime.click(function() {
+    console.log(sliderSessionSet + " is session")
+    console.log(sliderBreakSet + " is break")
+    startTime.css("display","none")
+    pauseTime.css("display","inline")
+    pause = false;
+    sliderSession.slider("disable")
+    sliderBreak.slider("disable")
+    if (!start) {
+      if (session) {
+        breakT.css("opacity", 0.1)
+        sliderSessionSet = sliderSession.slider("value");
+        sliderBreakSet = sliderBreak.slider("value");
+        initialTime = sliderSessionSet * 60;
+        console.log(sliderSessionSet + " is session")
+        console.log(sliderBreakSet + " is break")
+      } else {
+        sessT.css("opacity", 0.1)
+        sliderBreakSet = sliderBreak.slider("value");
+        sliderSessionSet = sliderSession.slider("value");
+        initialTime = sliderBreakSet * 60;
+        console.log(sliderSessionSet + " is session")
+        console.log(sliderBreakSet + " is break")
+      }
+      bell.play()
+      myVar = setInterval(beginTime, 1000);
+      start = true;
+    }
+  })
+
   function beginTime() {
     if (initialTime == 0) {
-      if (sessionBreak == false) {
-      $("#status").html("BREAK SESSION !");
-      audio2.play();
-      initialTime = breakTime;
-      sessionBreak = true;
-      return initialTime;
-    } else {
-      sessionBreak = false;
-      console.log(sessionBreak);
-      $("#status").html("SESSION");
-      initialTime = sessionTime;
-    }
-    }
-    console.log(initialTime);
-    initialTime--;
-    printTime();
-    document.getElementById("timeNow").innerHTML = (timeMinutes + " minutes and " + timeSeconds + " seconds left");
-    }
-    function stopTimeButton() {
-      clearInterval(myVar);
-      pushedOnce = false;
-    };
-    function clearTime() {
-      initialTime = 0;
-    }
-  $(document).ready(function() {
-    displayBreak();
-    printTime();
-    $("#timeNow").html(timeMinutes + " minutes and " + timeSeconds + " seconds left");
-    $("#clearTime").click(function () {
-      stopTimeButton();
-      initialTime = 0;
-      printTime();
-      displayTime();
-    });
-    $("#plusSession").click(function () {
-      stopTimeButton();
-      initialTime += 60;
-      printTime();
-      displayTime();
-      displaySession();
-    });
-    $("#minusSession").click(function () {
-      stopTimeButton();
-      if (initialTime > 0) {
-      initialTime -= 60;
+      civil.play()
+      // if session is false (breaktime) turn session opaque, play session
+      if (!session) {
+        breakT.css("opacity", 0.1)
+        sessT.css("opacity", 1)
+        console.log(sliderSessionSet + " is session")
+        console.log(sliderBreakSet + " is break")
+        initialTime = sliderSessionSet * 60
+        session = true;
+      } else {
+      // if session is true (sessiontime), turn break opaque, play break
+        breakT.css("opacity", 1)
+        sessT.css("opacity", 0.1)
+        console.log(sliderSessionSet + " is session")
+        console.log(sliderBreakSet + " is break")
+        initialTime = sliderBreakSet * 60
+        session = false;
       }
-      printTime();
-      displayTime();
-      displaySession();
-    });
-    $("#plusBreak").click(function () {
-      breakTime += 60;
-      displayBreak();
-    });
-    $("#minusBreak").click(function () {
-      if (breakTime > 0) {
-      breakTime -= 60;
-      }
-      displayBreak();
-    });
-});
+    }
+    if (!pause) {
+        initialTime--;
+        displayTime()
+    } 
+ 
+  }
+
+  pauseTime.click(function() {
+    civil.pause()
+    click.play()
+    pause = true;
+    startTime.css("display","inline")
+    pauseTime.css("display","none")
+  })
+
+  clearTime.click(function() {
+    clearInterval(myVar)
+    breakT.css("opacity", 1)
+    sessT.css("opacity", 1)
+    click.play()
+    startTime.css("display","inline")
+    pauseTime.css("display","none")
+    pause = true;
+    sliderSession.slider("enable")
+    sliderBreak.slider("enable")
+    amount3.val(0)
+    amount4.val(0)
+    start = false;
+    reset()
+  })
+
+
+})
