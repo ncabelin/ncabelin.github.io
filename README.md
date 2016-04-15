3073305f7ce17696d0c082ef9b967cb1789f5e8b
ncabelin@gmail.com <ncabelin@gmail.com>
2016-04-15 01:35:39 -0700

    new

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# ncabelin.github.io
+# My Personal Portfolio
+
+Here you can see my recent projects. I used github pages to host most of the static projects. 
+I used HTML5,CSS3,Twitter Bootstrap, jQuery and JavaScript for this webpage.
--- js/model.js
+++ js/model.js
@@ -1,69 +1,69 @@
 	var projects = [{
 		title: "Potentum Studios",
-		id: "potent",
+		id: "potentumStudios",
 		description: "My web design &amp; development company website. We design logos, set up web hosting and domain name registration. We can also provide custom in-house art and sound assets for your web ventures. We also provide Wordpress management and set-up.<br><br>I used HTML5 / CSS3, Javascript, jQuery and Twitter Bootstrap for this site.<br>",
 		url: "http://www.potentum.com",
 		image: "https://cloud.githubusercontent.com/assets/15892944/14021986/f57073d0-f19a-11e5-9546-d3077c7d5063.jpg",
 		github: "https://github.com/ncabelin/ncabelin.github.io/tree/master/potentum"
 	}, {
 		title: "LA Attractions Map",
-		id: "lamp",
+		id: "losAngelesAttractions",
 		description: "Neighborhood map of the Greater Los Angeles area and the top museums, theme parks, and other attractions.<br> Uses knockout.js framework, jQuery, Openweather API to get current LA weather, Google maps, places and streetview API, and wikipedia API to query info about the places. Currently only works with large viewports due to map interactions and info size.<br>",
 		url: "http://ncabelin.github.io/udneighbormap",
 		image: "https://cloud.githubusercontent.com/assets/15892944/14290947/f52ad8fa-fb15-11e5-84ad-1fa527adc8af.jpg",
 		github: "https://github.com/ncabelin/ncabelin.github.io/tree/master/udneighbormap"
 	}, {
 		title: "Weather Checker",
-		id: "weather",
+		id: "weatherChecker",
 		description: "Your quick and accurate weather app. Gets current weather forecast using gps coordinates geolocation from the device you are using. Open Weather uses data from your location's specific weather station and is the most accurate in predictions due to this.<br><br>Demonstrates use of AJAX calls with OpenWeather API using jQuery. Used HTML5,CSS3,Javascript,Bootstrap.<br>",
 		url: "http://ncabelin.github.io/fccweather",
 		image: "https://cloud.githubusercontent.com/assets/15892944/14010790/9c019594-f155-11e5-8b49-b131f704d625.jpg",
 		github: "https://github.com/ncabelin/ncabelin.github.io/tree/master/fccweather"
 	}, {
 		title: "Javascript Calculator",
-		id: "javasc",
+		id: "javascriptCalculator",
 		description: "jQuery &amp; Javascript based traditional calculator app using grid-based Bootstrap buttons, styled by CSS3.<br><br>I used HTML5,CSS3,Javascript,jQuery,Bootstrap.<br>",
 		url: "http://ncabelin.github.io/fcccalculator",
 		image: "https://cloud.githubusercontent.com/assets/15892944/14010789/9bf7a7d2-f155-11e5-8cce-83ac90cb22a9.jpg",
 		github: "https://github.com/ncabelin/ncabelin.github.io/tree/master/fcccalculator"
 	}, {
 		title: "Pomodoro Timer",
-		id: "pom",
+		id: "timer",
 		description: "Productivity timer app with sounds. Determine session time and breaks with the Pomodoro technique of time management. Session minutes are set to 25 and breaks are 5 minutes by default.<br>I used HTML5 / CSS3, Javascript jQuery &amp; jQuery UI with Touch Punch to make the sliders mobile-responsive. Try it on your phone, it feels like a native app because of Touch Punch.",
 		url: "http://ncabelin.github.io/fccpomodoro",
 		image: "https://cloud.githubusercontent.com/assets/15892944/14312600/c7a70958-fba1-11e5-8cf7-321fb743bf70.jpg",
 		github: "https://github.com/ncabelin/ncabelin.github.io/tree/master/fccpomodoro"
 	}, {
 		title: "Key Swiper Arcade Game",
-		id: "keyswipe",
+		id: "keyswiperGame",
 		description: "Arcade game in the vein of Frogger. Get past the bugs and fishes, swipe the key to score.<br><br>I used HTML5 canvas to generate the graphics and Javascript object-oriented programming to design the functionality.<br><br>",
 		url: "http://ncabelin.github.io/udfrog",
 		image: "https://cloud.githubusercontent.com/assets/15892944/14368462/af146c1e-fcd2-11e5-95df-b545e9242f88.jpg",
 		github: "https://github.com/ncabelin/ncabelin.github.io/tree/master/udfrog"
 	}, {
 		title: "Simon Game",
-		id: "simong",
+		id: "simonGame",
 		description: "Simon Game clone, the game popularized in the 80s. Try to imitate a randomly generated pattern by memory, the game increases the pattern length after each correct guess. You win when you get to the 20th pattern.<br><br>I used jQuery, Javascript, HTML5 audio to provide sounds for the game.<br><br>",
 		url: "http://ncabelin.github.io/fccsimon",
 		image: "https://cloud.githubusercontent.com/assets/15892944/14010786/9bf384f4-f155-11e5-941a-d8b87c8c48f3.jpg",
 		github: "https://github.com/ncabelin/ncabelin.github.io/tree/master/fccsimon"
 	}, {
 		title: "Tic Tac Toe",
-		id: "tictac",
+		id: "tictactoe",
 		description: "Play Tic Tac Toe against the computer. I designed the AI to be smart enough, but there is one pattern that can make it possible for the human player to win, perfect AI in progress.<br><br>I used jQuery, and vanilla Javascript for the AI, along with Bootstrap styling.",
 		url: "http://ncabelin.github.io/fcctic",
 		image: "https://cloud.githubusercontent.com/assets/15892944/14010785/9bf2c58c-f155-11e5-8993-f55acf4c2eff.jpg",
 		github: "https://github.com/ncabelin/ncabelin.github.io/tree/master/fcctic"
 	}, {
 		title: "Wikipedia Viewer",
-		id: "wikiv",
+		id: "wikiviewer",
 		description: "Search Wikipedia quickly. This app uses Wikimedia API to query topics and shows the top 20 results.<br><br>Demonstrates knowledge of API uses, AJAX calls and is styled with Bootstrap,CSS3.",
 		url: "http://ncabelin.github.io/fccwiki",
 		image: "https://cloud.githubusercontent.com/assets/15892944/14010784/9bf0d27c-f155-11e5-8455-066293c4f734.jpg",
 		github: "https://github.com/ncabelin/ncabelin.github.io/tree/master/fccwiki"
 	}, {
 		title: "Feed Reader Testing",
-		id: "feedR",
+		id: "feedReaderTester",
 		description: "RSS Feed reader of Udacity, contains Jasmine.js testing. Demonstrates knowledge of red-green factor, a/b testing.",
 		url: "http://ncabelin.github.io/udfeed",
 		image: "https://cloud.githubusercontent.com/assets/15892944/14395568/4b3312e4-fd87-11e5-8fee-f68b2fc16be0.jpg",
