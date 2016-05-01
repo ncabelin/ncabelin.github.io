bd47e90242cebbedafbdcf4786d55e3ad79269bb
ncabelin@gmail.com <ncabelin@gmail.com>
2016-05-01 01:36:06 -0700

    k

---
+++ pul/js/controller.js
@@ -0,0 +1,202 @@
+var app = {
+  // results of GPS calculation
+  results: [],
+  // coordinates
+  latCheck: 0,
+  lngCheck: 0,
+  //length: locations.length,
+  table: $('#tableSource'),
+  loading: function() {
+      $('#loading').toggle();
+  },
+  startApp: function() {
+    var checkGPSbtn = $('#checkGPS');
+    var checkAddbtn = $('#checkAddress');
+    var inputAdd = $('#inputAddress');
+    var listAllBtn = $('#listAll');
+    var contacts = $('#contacts');
+    var help = $('#help');
+    var options, userList;
+
+    this.listAll();
+    this.modalSet($('td.name'));
+
+    listAllBtn.click(function() {
+      app.table.empty();
+      app.listAll();
+    });
+
+    help.click(function() {
+      $('.modal-title').html('Instructions');
+      var content = '<ol><li>To check nearest psych unit locations, click <strong>Locate by GPS button</strong></li>' +
+        '<li>To check psych unit nearest to a zipcode, type zipcode in the input field <br>and click <i class="fa fa-search"></i></li>' +
+        '<li>Click menu button List to display all data without finding out distance</li>' +
+        '<li>To filter table put searchword in the input box labeled Filter List</li>' +
+        '<li>Click psych unit name to access more info (e.g. telephone numbers)</li></ol>';
+      $('.modal-body').html(content);
+      $('#myModal').modal();
+    });
+
+    // check user's geographic coordinates to find miles away.
+    checkGPSbtn.click(function() {
+      app.loading();
+      app.results = [];
+      app.checkGPS();
+    });
+
+    // check user's inputted address to find miles distance of locations.
+    checkAddbtn.click(function() {
+      var name = inputAdd.val();
+      var isValidZip = /(^\d{5}$)|(^\d{5}-\d{4}$)/.test(name);
+      if (isValidZip) {
+        app.message('');
+        app.loading();
+        app.table.empty();
+        app.results = [];
+        app.checkAddress(locations, name);
+        app.modalSet();
+      } else {
+        app.message('Invalid zipcode, please check again');
+        setTimeout(function() { app.message(''); }, 5000);
+      }
+    });
+
+  },
+
+  modalSet: function(className) {
+    className.click(function(data) {
+      console.log('click');
+      var text = this.textContent;
+      locations.forEach(function(data, index) {
+        if (data.name == text) {
+          $('.modal-title').html(text);
+          var content = data.info + '<br><br><em>' + data.address + '</em><br><a href="tel:' + data.tel + '">' + data.tel + '</a>';
+          $('.modal-body').html(content);
+          $('#myModal').modal();
+        }
+      });
+    });
+  },
+
+  // delivers error messages
+  message: function(msg) {
+    $('.message').html(msg);
+  },
+
+  listAll: function() {
+    app.addTableDiv(app.table, 'info', 'Info');
+
+    // set-up first table with name,info and address by default
+    locations.forEach(function(data) {
+      $('.list').append('<tr><td class="name">' + data.name + '</td><td class="info">' + data.info +
+        '</td><td>' + data.address + '</td></tr>');
+    });
+
+    // list.js parameters set-up
+    options = { valueNames: [ 'name', 'info', 'tel' ] };
+    userList = new List('tableSource', options);
+  },
+
+  // adds table to id, and puts sortable val with valDesc as placeholder
+  addTableDiv: function(id, val, valDesc) {
+    console.log('adding Table');
+    id.append('<input class="search text-center center-block" placeholder="Filter List"><h4 class="text-center">Sort by : ' +
+    '<button class="sort btn btn-info" data-sort="name">Name</button> <button id="val" class="sort btn btn-info" ' +
+    'data-sort="' + val + '">' + valDesc + '</button></h4><table><thead></thead>' +
+    '<tbody class="list"></tbody></table>');
+  },
+
+  // populates table
+  createTable: function(table) {
+    app.results.forEach(function(data, index) {
+      console.log('creating Table ' + index);
+      $('.list').append('<tr><td class="name">' + data.name + '</td><td class="distance">' +
+        data.distance + '</td></tr>');
+    });
+    options = { valueNames: [ 'name', 'distance'] };
+    userList = new List('tableSource', options);
+  },
+
+  // checks 2 coordinates' distance
+  checkDistance: function(lat1, lon1, lat2, lon2, unit) {
+    console.log('checking Distance');
+    var radlat1 = Math.PI * lat1/180;
+    var radlat2 = Math.PI * lat2/180;
+    var theta = lon1-lon2;
+    var radtheta = Math.PI * theta/180;
+    var dist = Math.sin(radlat1) * Math.sin(radlat2) + Math.cos(radlat1) * Math.cos(radlat2) * Math.cos(radtheta);
+    dist = Math.acos(dist);
+    dist = dist * 180/Math.PI;
+    dist = dist * 60 * 1.1515;
+    if ( unit == 'K') { dist = dist * 1.609344; }
+    if ( unit == 'N') { dist = dist * 0.8684; }
+    return dist;
+  },
+
+  // checks geolocation coordinates
+  checkGPS: function() {
+    console.log('checking GPS');
+    if (navigator.geolocation) {
+      // ask for gps coordinates
+      navigator.geolocation.getCurrentPosition(function(position) {
+        app.lngCheck = position.coords.longitude;
+        app.latCheck = position.coords.latitude;
+        app.table.empty();
+        app.calcResults(locations);
+        app.loading();
+        app.modalSet($('.name'));
+        if (app.lngCheck === 0) {
+          console.log('error');
+        }
+      });
+    } else {
+      app.message('Error, no geolocator found in your device');
+    }
+  },
+
+  checkAddress: function(data3, name) {
+    console.log('checking Address');
+    var geocoder = new google.maps.Geocoder();
+    geocoder.geocode({'address': name}, function(results, status) {
+      if (status === google.maps.GeocoderStatus.OK) {
+        app.lngCheck = results[0].geometry.location.lng();
+        app.latCheck = results[0].geometry.location.lat();
+        app.calcResults(locations);
+        app.loading();
+        app.modalSet($('.name'));
+        if (app.lngCheck === 0) {
+          console.log('error');
+        }
+      } else {
+        app.message('Geocode was not successful for the following reason: ' + status);
+      }
+    });
+  },
+
+  calcResults: function(data2) {
+    console.log('calculating results');
+    for (var i = 0; i < 33; i++) {
+      var dist = Math.floor(app.checkDistance(app.latCheck, app.lngCheck, data2[i].lat, data2[i].long)) + ' miles';
+      app.results.push({
+        distance: dist,
+        name: data2[i].name,
+        address: data2[i].address,
+        tel: data2[i].tel,
+        info: data2[i].info
+      });
+      if (i == 32) {
+        app.addTableDiv(app.table, 'distance', 'Distance');
+        app.createTable();
+        $('#val').trigger('click');
+      }
+    }
+  }
+};
+
+function callSuccess() {
+  app.startApp(); // start App
+}
+
+function callError() {
+  app.message('Error loading google Maps Places API'); // call Error
+}
---
+++ pul/js/model.js
@@ -0,0 +1,245 @@
+var locations = [
+{
+  'name': 'Northridge Hospital-Roscoe Campus',
+  'address': '18300 Roscoe Blvd. Northridge, CA 91328',
+  'tel': '(818)885-8500',
+  'info': 'Medicare',
+  'lat': 34.2200955,
+  'long': -118.5353023
+},{
+  'name': 'Pacifica Hospital of the Valley',
+  'address': '9449 San Fernando Rd. Sun Valley, CA 91352',
+  'tel': '(800)522-1154',
+  'info': 'Fee for Service Medi-cal',
+  'lat': 34.2400992,
+  'long': -118.3986849
+},{
+  'name': 'Glendale Adventist',
+  'address': '1509 Wilson Terrace Glendale, CA 91206',
+  'tel': '(818)409-8000',
+  'info': 'Fee for Service Medi-cal',
+  'lat': 34.1504441,
+  'long': -118.2321505
+},{
+  'name': 'Glendale Memorial Hospital',
+  'address': '1420 South Central Ave. Glendale, CA 91204',
+  'tel': '(818)502-2362',
+  'info': 'Adults-Voluntary Admissions only & has LPS destignation.<br>Must have medical clearance first.<br>They do not provide transportation.',
+  'lat': 34.1281181,
+  'long': -118.2591006
+},{
+  'name': 'Olive View Medical Center',
+  'address': '14445 Olive View Dr. Sylmar, CA 91342',
+  'tel': '(818)364-1555',
+  'info': 'County',
+  'lat': 34.3257364,
+  'long': -118.4479592
+},{
+  'name': 'Van Nuys Community Hospital',
+  'address': '14433 Emelita St. Van Nuys, CA 91401',
+  'tel': '(800)565-0558',
+  'info': 'Fee for Service Medi-Cal.<br>A Campus of Hollywood Community Hospital, Voluntary Admissions Only.<br>Patient info number is 818-787-1511',
+  'lat': 34.177062,
+  'long': -118.449683
+},{
+  'name': 'Hollywood Community Hospital',
+  'address': '6245 De Longpre Ave. Hollywood, CA 90028',
+  'tel': '(323)462-2271',
+  'info': 'Fee for Service Medi-cal.<br>Hollywood Campus.<br>Accepts Voluntary Admissions',
+  'lat': 34.0964452,
+  'long': -118.3273379
+},{
+  'name': 'Brotman Medical Center',
+  'address': '3828 Delmas Terrace Culver City, CA 90232',
+  'tel': '(310)836-7000',
+  'info': 'Fee for Service Medi-cal Adult & Geriatric<br>(Accepts 5150 with co-occuring medical conditions)<br>tel.extension 6600 or 1(800)565-0558.',
+  'lat': 34.0231953,
+  'long': -118.3991369
+},{
+  'name': 'Los Angeles Community Hospital',
+  'address': '4081 East Olympic Blvd. Los Angeles, CA 90023',
+  'tel': '(323)267-0477',
+  'info': 'Fee for Service Medi-cal.<br>Accepts Voluntary Admissions.',
+  'lat': 34.0193964,
+  'long': -118.1889139
+},{
+  'name': 'Aurora Charter Oak',
+  'address': '1161 East Covina Blvd. Covina, CA 91724',
+  'tel': '(626)966-1632',
+  'info': 'Fee for Service Medi-cal.<br>Medi-cal & Medicare<br>1(800)792-2345<br>Children & adolescent(5+)<br>& Geropsychiatric Program(55+)(20-Bed Unit)',
+  'lat': 34.1003274,
+  'long': -117.8690013
+},{
+  'name': 'Aurora Las Encinas Hospital',
+  'address': '2900 E Del Mar Blvd, Pasadena, California 91107-4375',
+  'tel': '(800)792-2345',
+  'info': 'Does not accept Adult Medi-cal Ages 21-65.<br>Adolescent (GLBT Alternatives Unit)<br>(Accepts both voluntary & involuntary admissions)',
+  'lat': 34.1418272,
+  'long': -118.0934769
+},{
+  'name': 'Antelope Valley Hospital',
+  'address': '1600 West Ave. J Lancaster, CA 93534',
+  'tel': '(661)949-5000',
+  'info': 'Fee for service Medi-cal.<br>Does not accept clients from outside the Antelope Valley area.',
+  'lat': 34.6879037,
+  'long': -118.1601727
+},{
+  'name': 'Huntington Hospital-Della Martin Ctr.',
+  'address': '100 West California Blvd. Pasadena, CA 91109',
+  'tel': '(626)397-5000',
+  'info': 'Fee for service Medi-cal',
+  'lat': 34.1332928,
+  'long': -118.1549523
+},{
+  'name': 'BHC Alhambra',
+  'address': '4619 North Rosemead Blvd. Rosemead, CA 91770',
+  'tel': '(626)286-1191',
+  'info': 'Does not accept Adult Medi-cal',
+  'lat': 34.0887489,
+  'long': -118.0760877
+},{
+  'name': 'LAC-USC Medical Center',
+  'address': '1200 North State St. Los Angeles, CA 90033',
+  'tel': '(323)409-7085',
+  'info': 'County',
+  'lat': 34.0577573,
+  'long': -118.2103504
+},{
+  'name': 'A Hawkins',
+  'address': '1720 East 120th St. Los Angeles, CA 90018',
+  'tel': '(310)668-4271',
+  'info': 'County',
+  'lat': 33.9240672,
+  'long': -118.2444949
+},{
+  'name': 'LA Metropolitan Medical Center',
+  'address': '2231 South Western Ave. Los Angeles, CA 90018',
+  'tel': '(323)730-7300',
+  'info': 'Fee for Service Medi-cal',
+  'lat': 34.0351431,
+  'long': -118.3115527
+},{
+  'name': 'White Memorial Medical Center',
+  'address': '1720 Cesar E. Chavez Ave. Los Angeles, CA 90033',
+  'tel': '(323)265-5050',
+  'info': 'Fee for Service Medi-cal',
+  'lat': 34.0492542,
+  'long': -118.219434
+},{
+  'name': 'Mission Community Hospital-Panorama Campus',
+  'address': '14850 Roscoe Blvd. Panorama City, CA 91402',
+  'tel': '(800)608-4624',
+  'info': 'Fee for Service Medi-cal',
+  'lat': 34.2204258,
+  'long': -118.4587374
+},{
+  'name': 'Henry Mayo Newhall Memorial Hospital',
+  'address': '23845 McBean Parkway Valencia, CA 91355',
+  'tel': '(661)253-8000',
+  'info': 'Fee for Service Medi-cal & Medi-care',
+  'lat': 34.4214827,
+  'long': -118.5654093
+},{
+  'name': 'Henry Mayo Newhall Behavioral Health Services',
+  'address': '25727 McBean Parkway Valencia, CA 91355',
+  'tel': '(661)253-8954',
+  'info': 'Fee for Service Medi-cal & Medi-care',
+  'lat': 34.4214827,
+  'long': -118.5654093
+},{
+  'name': 'County of Los Angeles Harbor-UCLA Med.Ctr.',
+  'address': '1000 W. Carson St. Torrance, CA 90509',
+  'tel': '(310)222-3144',
+  'tel2': '(310)222-2345',
+  'info': 'County',
+  'lat': 33.8307253,
+  'long': -118.2940334
+},{
+  'name': 'College Hospital-Cerritos',
+  'address': '10802 College Place Cerritos, CA 90703',
+  'tel': '(562)924-9581',
+  'tel2': '(800)352-3301',
+  'info': 'Geriatric(60+),Adults,Adolescent (13-17),& Pediatric(2-12)<br>CRISIS TEAM(800)352-3301',
+  'lat': 33.8846153,
+  'long': -118.1052306
+},{
+  'name': 'College Hospital-Costa Mesa',
+  'address': '301 Victoria St. Costa Mesa, CA 92627',
+  'tel': '(949)642-2734',
+  'info': '',
+  'lat': 33.6518275,
+  'long': -117.9142578
+},{
+  'name': 'Verdugo Hills Hospital',
+  'address': '1812 Verdugo Blvd. Glendale, CA 91208',
+  'tel': '(818)952-2270',
+  'info': 'Medicare only 52+',
+  'lat': 34.2045351,
+  'long': -118.2182377
+},{
+  'name': 'Encino/Tarzana Hospital',
+  'address': '18321 Clark St. Tarzana, CA 91356',
+  'tel': '(818)995-5155',
+  'info': 'Medicare only 55+.<br>Admitting (730-4pm)M-F. Clients must be taken to ER for clearance<br>first before admission.They do not have a response team that provides<br>"field-based" emergency services.',
+  'lat': 34.1704136,
+  'long': -118.5344612
+},{
+  'name': 'Sherman Oaks Hospital',
+  'address': '4929 Van Nuys Blvd. Sherman Oaks, CA 91403',
+  'tel': '(818)981-7111',
+  'info': 'Medicare only 55+, Voluntary Admissions Only',
+  'note': 'extension 4000',
+  'lat': 34.1602305,
+  'long': -118.4518215
+},{
+  'name': 'Del Amo Behavioral Health',
+  'address': '23700 Camino de Sol Torrance, CA 90505',
+  'tel': '(800)533-5266',
+  'tel2': '(310)530-1151',
+  'info': 'Medi-cal & Medicare, Geriatric (65+)<br>Adults,Adolescent & Pediatric (6+)',
+  'note': 'extension 200',
+  'lat': 33.8103056,
+  'long': -118.3466698
+},{
+  'name': 'Gateways Hospital & Mental Health Center',
+  'address': '1891 Effie St. Los Angeles, CA 90026',
+  'tel': '(800)533-5266',
+  'tel2': '(323)644-2000',
+  'info': 'Short Doyle only.Adult & Adolescent',
+  'note': 'extension 274',
+  'lat': 34.085007,
+  'long': -118.2587249
+},{
+  'name': 'Canyon Ridge Hospital',
+  'address': '5353 G. Street Chino, CA 91710',
+  'tel': '(909)590-3700',
+  'info': 'Medi-cal & Medicare, Adult & Adolescent',
+  'note': '',
+  'lat': 34.0090592,
+  'long': -117.690239
+},{
+  'name': 'San Gabriel Valley Medical Center',
+  'address': '438 West Las Tunas Dr. San Gabriel, CA 91776',
+  'tel': '(626)300-7300',
+  'info': 'Medi-cal & Medicare, Geriatric 60+<br>(38 bed locked inpatient psychiatric unit-involuntary<br>and voluntary patients accepted)',
+  'note': '',
+  'lat': 34.1007416,
+  'long': -118.1080642
+},{
+  'name': 'Samuel Goldwyn Jr. Center for Behavioral Health',
+  'address': '23388 Mulholland Dr. Woodland Hills, CA 91364',
+  'tel': '(818)876-4140',
+  'info': '12 beds',
+  'note': '',
+  'lat': 34.1569807,
+  'long': -118.6379289
+},{
+  'name': 'LA County Dept. of Mental Health<br>Olive View Community Mental Health Urgent Care Ctr.',
+  'address': '14659 Olive View Dr. Sylmar, CA 91342',
+  'tel': '(818)485-0888',
+  'info': 'hours of operation : M-F 8am - 10pm, Sat & Sun 9am - 5:30pm',
+  'note': '',
+  'lat': 34.1569807,
+  'long': -118.6379289
+}
+];
