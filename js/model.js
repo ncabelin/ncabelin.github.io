356b0e2fa7d2c85fc0e7830344063c733505ac78
Michael Cabelin <ncabelin@gmail.com>
2016-05-22 01:18:12 -0700

    revised PUL image

--- js/model.js
+++ js/model.js
@@ -3,7 +3,7 @@
 		id: "puL",
 		description: "Mental health services locator used by SFVMHC for locating the nearest facilities for their clients while on the field.<br><br>Uses Google Map Places API for geocoding, Device geolocator, Coordinates for each facilities. ListJS for table sorting and algorithm for calculating distances between two coordinates.",
 		url: "http://plocator.mindwelder.com",
-		image: "https://cloud.githubusercontent.com/assets/15892944/15452987/00843e9c-1fb7-11e6-9424-b511f695b7b1.jpg",
+		image: "https://cloud.githubusercontent.com/assets/15892944/15453057/f0663db8-1fba-11e6-9ed7-155ecc1140bb.jpg",
 		github: "https://github.com/ncabelin/psych-unit-locator-LA"
 	}, {
 		title: "Potentum Studios",
