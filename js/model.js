d16c119a74654f165a98fa6ce435ea190e4f7de3
Michael Cabelin <ncabelin@gmail.com>
2016-12-29 01:58:40 -0800

    new UI UX content

--- js/model.js
+++ js/model.js
@@ -2,55 +2,47 @@
 		title: "L.A. County Psych-unit Locator",
 		id: "puL",
 		description: "Mental health services locator currently used by the San Fernando Valley Community Mental Health Center Inc. for locating the nearest facilities for their clients while on the field by device GPS or zipcode.<br><br>Uses Google Maps API served through a NodeJS Backend Service API, Firebase Backend for Authentication &amp; Database and AngularJS front-end.",
-		sub: ["(Full Stack Application) using", "AngularJS 1.x", "NodeJS micro-service in Heroku", "Firebase Backend/Database", "Google Maps API"],
+		sub: ["AngularJS 1.x", "NodeJS via Heroku", "Firebase Backend/Database", "Google Maps API"],
 		url: "http://plocator.mindwelder.com",
 		image: "https://cloud.githubusercontent.com/assets/15892944/18023119/f53124da-6ba9-11e6-9a4c-cf9feaf7de4c.png",
 		github: "https://github.com/ncabelin/psych-unit-locator-LA"
-	},{
-		title: "Mindwelder: Flashcard App",
-		id: "flashcards",
-		description: "Fullstack Flashcard App where users can sign up, log in and add their own flashcards and flashcard sets for self-testing. Ability to use Leitner method of learning incorporated.<br><br>Uses PHP 7.0, MySQL and jQuery, HTML5/CSS3.",
-		sub: ["(Full Stack Application) using", "PHP", "MySQL", "jQuery", "HTML5/CSS3", "Bootstrap", "List.JS"],
-		url: "http://cards.mindwelder.com",
-		image: "https://cloud.githubusercontent.com/assets/15892944/17835927/649610d8-6736-11e6-98b4-c2c1726b6f47.png",
-		github: "https://github.com/ncabelin/flashcards-mindwelder"
+	// },{
+	// 	title: "Mindwelder: Flashcard App",
+	// 	id: "flashcards",
+	// 	description: "Fullstack Flashcard App where users can sign up, log in and add their own flashcards and flashcard sets for self-testing. Ability to use Leitner method of learning incorporated.<br><br>Uses PHP 7.0, MySQL and jQuery, HTML5/CSS3.",
+	// 	sub: ["(Full Stack Application) using", "PHP", "MySQL", "jQuery", "HTML5/CSS3", "Bootstrap", "List.JS"],
+	// 	url: "http://cards.mindwelder.com",
+	// 	image: "https://cloud.githubusercontent.com/assets/15892944/17835927/649610d8-6736-11e6-98b4-c2c1726b6f47.png",
+	// 	github: "https://github.com/ncabelin/flashcards-mindwelder"
 	},{
 		title: "Mindwelder Blogs",
 		id: "blogs",
-		description: "Fullstack Blog Application with user registration and ability to post blogs with html formatting tags, upload imgur picture links, code lines in the pre tag, etc.",
-		sub: ["(Static Website) using", "Google App Engine", "Python", "Webapp2", "Google Datastore NoSQL"],
-		url: "https://blog.mindwelder.com",
-		image: "https://cloud.githubusercontent.com/assets/15892944/19257141/157089fc-8f22-11e6-8341-de99ded939b9.jpg",
-		github: "https://github.com/ncabelin/python-blog"
+		description: "Fullstack Blog Application with user registration and the ability to post blogs with formatting, upload imgur picture links, use html formatting. A unique feature is the Testing mode. Underline style a section and it will be designated as an answer for a fill-in-the-blank type question. Tests can be saved for continuation later on. The web app also uses Oauth2 from Google+ and Facebook.",
+		sub: ["Python", "Flask", "PostgreSQL", "jQuery", "DigitalOcean VPS"],
+		url: "https://www.mindwelder.com",
+		image: "https://cloud.githubusercontent.com/assets/15892944/21518633/bef3ef7a-cc9c-11e6-975b-a7fe145d21bb.jpg",
+		github: "https://github.com/ncabelin/mindwelder"
 	},{
 		title: "Progressive Steps, Inc.",
 		id: "progressive",
-		description: "Website designed for Early Childhood Development Provider.<br><br>Used HTML5 / CSS3, Javascript, jQuery",
-		sub: ["(Static Website) using", "HTML5/CSS3", "jQuery", "Bootstrap"],
-		url: "http://progressive.potentum.com",
+		description: "Custom Wordpress website designed for an Early Childhood Development Provider. Based on the client's needs, initially created mockup of site in HTML5/CSS3 and Bootstrap, but the client ultimately wanted ease of editing features and we migrated the site to Wordpress.",
+		sub: ["Wordpress", "PHP", "X-theme", "Photoshop", "HTML5/CSS3", "MySQL"],
+		url: "https://www.progressivestepsca.com",
 		image: "https://cloud.githubusercontent.com/assets/15892944/16222217/df3dd386-374b-11e6-88b2-7a7843e3632d.jpg",
-		github: "https://github.com/ncabelin/progressivesteps"
+		github: ""
 	}, {
 		title: "LA Attractions Map",
 		id: "losAngelesAttractions",
-		description: "Neighborhood map of the Greater Los Angeles area and the top museums, theme parks, and other attractions.<br> Uses knockout.js framework, jQuery, Openweather API to get current LA weather, Google maps, places and wikipedia API to query info about the places.<br>",
-		sub: ["(Front-end Web App) using", "KnockoutJS", "jQuery", "Openweather API", "Google Maps API", "Wikipedia API"],
+		description: "Neighborhood map of the Greater Los Angeles area and the top museums, theme parks, and other attractions.",
+		sub: ["KnockoutJS", "jQuery", "Openweather API", "Google Maps API", "Wikipedia API"],
 		url: "http://ncabelin.github.io/udneighbormap",
 		image: "https://cloud.githubusercontent.com/assets/15892944/14626721/eeb4e30c-05a2-11e6-9fdf-5c216439bc9f.jpg",
 		github: "https://github.com/ncabelin/ncabelin.github.io/tree/master/udneighbormap"
 	}, {
-		title: "Weather Saver",
-		id: "weatherSaver",
-		description: "Gets current weather forecast using geolocation coordinates from the device you are using. The Open Weather API uses data from your location's specific weather station.<br><br>Demonstrates use of AJAX calls with OpenWeather API using AngularJS and saving to a NodeJS &amp; MongoDB backend<br>",
-		sub: ["(Front-end Web App) using", "AngularJS 1.x", "NodeJS", "D3.js", "Openweather", "API", "jQuery", "Bootstrap"],
-		url: "http://weather.potentum.com",
-		image: "https://cloud.githubusercontent.com/assets/15892944/19469361/e5e40cfa-94cf-11e6-9ca4-c788cdcf90b6.jpg",
-		github: "https://github.com/ncabelin/weather-saver"
-	}, {
 		title: "Talking Calculator",
 		id: "talkingCalculator",
-		description: "Use the Talking Calculator app for breaking down communication/disability barriers. This app uses grid-based Bootstrap buttons and currently only supports the English language.<br><br>I used HTML5,CSS3,Javascript,jQuery,Bootstrap and responsiveVoiceJS for the text-to-speech functionality.<br>",
-		sub: ["(Front-end Web App) using", "responsiveVoiceJS", "HTML5/CSS3", "Javascript", "jQuery", "Bootstrap"],
+		description: "Use the Talking Calculator app for breaking down communication/disability barriers. This app uses grid-based Bootstrap buttons and currently only supports the English language.",
+		sub: ["responsiveVoiceJS", "HTML5/CSS3", "JavaScript", "jQuery", "Bootstrap"],
 		url: "http://ncabelin.github.io/fcccalculator",
 		image: "https://cloud.githubusercontent.com/assets/15892944/15271958/6fbfb82c-1a17-11e6-8070-98242c49036f.jpg",
 		github: "https://github.com/ncabelin/ncabelin.github.io/tree/master/fcccalculator"
@@ -66,6 +58,14 @@ var frontendProjects = [
 	image: "https://cloud.githubusercontent.com/assets/15892944/14021986/f57073d0-f19a-11e5-9546-d3077c7d5063.jpg",
 	github: "https://github.com/ncabelin/ncabelin.github.io/tree/master/potentum"
 },{
+		title: "Weather Saver",
+		id: "weatherSaver",
+		description: "Gets current weather forecast using geolocation coordinates from the device you are using. The Open Weather API uses data from your location's specific weather station.<br><br>Demonstrates use of AJAX calls with OpenWeather API using AngularJS and saving to a NodeJS &amp; MongoDB backend<br>",
+		sub: "Openweather, jQuery, NodeJS",
+		url: "http://weather.potentum.com",
+		image: "https://cloud.githubusercontent.com/assets/15892944/19469361/e5e40cfa-94cf-11e6-9ca4-c788cdcf90b6.jpg",
+		github: "https://github.com/ncabelin/weather-saver"
+	}, {
 		title: "Pomodoro Timer",
 		id: "timer",
 		description: "Productivity timer app with sounds. Determine session time and breaks with the Pomodoro technique of time management. Session minutes are set to 25 and breaks are 5 minutes by default.<br>I used HTML5 / CSS3, Javascript jQuery &amp; jQuery UI with Touch Punch to make the sliders mobile-responsive. Try it on your phone, it feels like a native app because of Touch Punch.",
@@ -86,12 +86,7 @@ var frontendProjects = [
 		url: "http://ncabelin.github.io/udfeed",
 		image: "https://cloud.githubusercontent.com/assets/15892944/14395568/4b3312e4-fd87-11e5-8fee-f68b2fc16be0.jpg",
 		github: "https://github.com/ncabelin/ncabelin.github.io/tree/master/udfeed"
-	}
-
-];
-
-var games = [
-	{
+	},{
 		title: "Key Swiper Arcade Game",
 		id: "keyswiperGame",
 		description: "Arcade game in the vein of Frogger. Get past the bugs and fishes, swipe the key to score.<br><br>I used HTML5 canvas to generate the graphics and Javascript object-oriented programming to design the functionality.<br><br>",
@@ -113,6 +108,10 @@ var games = [
 		image: "https://cloud.githubusercontent.com/assets/15892944/14010785/9bf2c58c-f155-11e5-8993-f55acf4c2eff.jpg",
 		github: "https://github.com/ncabelin/ncabelin.github.io/tree/master/fcctic"
 	}
+
+];
+
+var games = [
 ];
 
 var backendProjects = [{
