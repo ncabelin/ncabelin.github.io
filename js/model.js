a8571dade445bb24526599974eede3722abddfc7
Michael Cabelin <ncabelin@gmail.com>
2017-01-12 19:44:44 -0800

    update about me section

--- js/model.js
+++ js/model.js
@@ -33,7 +33,7 @@
 	}, {
 		title: "LA Attractions Map",
 		id: "losAngelesAttractions",
-		description: "Neighborhood map of the Greater Los Angeles area and the top museums, theme parks, and other attractions.",
+		description: "Neighborhood map of the Greater Los Angeles area and the top museums, theme parks, and other attractions. Uses Google maps API to view if currently open, Openweather to get current L.A. weather, Wikipedia API to provide info about each attraction",
 		sub: ["KnockoutJS", "jQuery", "Openweather API", "Google Maps API", "Wikipedia API"],
 		url: "http://cabelin.com/losangelesattractions",
 		image: "https://camo.githubusercontent.com/502df357b92ab9ddfe03ab77167d3b540dd566af/687474703a2f2f636162656c696e2e636f6d2f6c6f73616e67656c657361747472616374696f6e732f696d616765732f73637265656e73686f742e6a7067",
